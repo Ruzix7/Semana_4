2a9f78e604e5b2d2907a9c74e6a5dfc8d3bd8d1d
Sebastian Portillo <ruzixps@gmail.com>
2025-04-21 03:44:38 -0500

    New Project

---
+++ Plant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Semana_4
+{
+    internal class Plant
+    {
+        private string name;
+        private int lifeTime;
+        private int productionAmount;
+        private int seedPrice;
+        private int productPrice;
+
+        private int currentLifeTime;
+
+        public string Name { get { return name; } }
+        public int SeedPrice { get { return seedPrice; } }
+
+        public Plant(string name, int lifeTime, int productionAmount, int seedPrice, int productPrice)
+        {
+            this.name = name;
+            this.lifeTime = lifeTime;
+            this.productionAmount = productionAmount;
+            this.seedPrice = seedPrice;
+            this.productPrice = productPrice;
+            currentLifeTime = 0;
+        }
+
+        public void PassTurn()
+        {
+            currentLifeTime++;
+        }
+
+        public bool CanHarvest()
+        {
+            return currentLifeTime >= lifeTime;
+        }
+
+        public int GetTotalSellMoney()
+        {
+            return productionAmount * productPrice;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Semana_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FarmMenu menu = new FarmMenu();
+            menu.Execute();
+        }
+    }
+}
